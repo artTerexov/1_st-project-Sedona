ed5e96cdcd82f059e447397bd23b6f414e50fcc6
artTerexov <36787267+artTerexov@users.noreply.github.com>
2018-03-27 22:57:58 +0300

    js  в отдельный файл

---
+++ js/script.js
@@ -0,0 +1,7 @@
+	var button = document.querySelector(".hotels-search-button");
+    var popup = document.querySelector(".modal-search");
+
+    button.addEventListener("click", function (evt) {        
+    popup.classList.toggle("modal-show");
+    });
+  
