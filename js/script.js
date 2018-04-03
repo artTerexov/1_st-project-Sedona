de5289a83ae3d80104efcfbdad69fda7604aec1e
artTerexov <36787267+artTerexov@users.noreply.github.com>
2018-04-03 18:29:05 +0300

    Добавил анимацию формы и карту+ исправил оформление js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,44 @@
-var button = document.querySelector(".hotels-search-button");
+var link = document.querySelector(".hotels-search-button");
 var popup = document.querySelector(".modal-search");
+var form = popup.querySelector("form");
+var arrival = popup.querySelector("[name=arrival-date]");
+var departure = popup.querySelector("[name=departure-date]");
+var children = popup.querySelector("[name=count-children]");
+var adult = popup.querySelector("[name=count-adult]");
 
-button.addEventListener("click", function (evt) {        
+var isStorageSupport = true;
+var storageAdults = "";
+var storageChildrens = "";
+var storageArrival = "";
+var storageDeparture = "";
+
+try {
+  storageAdults = localStorage.getItem("adult");
+  storageChildrens = localStorage.getItem("children");
+  storageArrival = localStorage.getItem("arrival");
+  storageDeparture = localStorage.getItem("departure");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();        
   popup.classList.toggle("modal-close");
+  popup.classList.remove("modal-error");  
 });
-  
+
+
+form.addEventListener("submit", function (evt) {  
+  if (!arrival.value || !departure.value || !children.value || !adult.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");    
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");       
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("arrival", arrival.value);
+      localStorage.setItem("departure", departure.value);
+      localStorage.setItem("adult", adult.value);
+      localStorage.setItem("children", children.value); }
+  }
+});
